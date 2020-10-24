5783477e44d73f9250e512ed70ac106adac7556e
Julian B <julian.blazek@outlook.de>
2020-10-24 18:23:36 +0200

    Add weather generator

--- datagenerator-wetter/index.js
+++ datagenerator-wetter/index.js
@@ -1,11 +1,12 @@
 const { Kafka } = require('kafkajs');
 const { v4: uuidv4 } = require('uuid');
-
+const purpose = "weather";
 const uuid = uuidv4();
+const random = require('random');
 console.log("Producer started, UUID: " + uuid);
 
 const kafka = new Kafka({
-    clientId: 'wetter-' + uuid,
+    clientId: purpose + '-' + uuid,
     brokers: ['kafka1:19091', 'kafka2:19092', 'kafka3:19093']
 })
 
@@ -17,26 +18,36 @@ function sleep(ms) {
 }
 
 const run = async () => {
-    // await admin.createTopics({
-    //     topics: [{
-    //         topic: "Topic1",
-    //         replicationFactor: 3
-    //     }],
-    //     waitForLeaders: true
-    // }).catch();
-    await sleep(10);
     await producer.connect();
-    for(var i = 0; i < 10; i++) {
+
+    const current_temp = random.normal(21, 0.04);
+
+    await sleep(random.int(1000,10000)); // initial nap :)
+    while (true) {
         await producer.send({
-            topic: 'Topic1',
+            topic: 'comms.status.0',
             messages: [
-                {value: uuid + ': ' + i}
+                {value: JSON.stringify({event: 'JOIN', device: uuid, purpose: purpose})}
+            ]
+        });
+        
+        await producer.send({
+            topic: 'data.weather.0',
+            messages: [
+                {value: JSON.stringify({device: uuid, sensor: 'temp', data: current_temp()})}
             ]
         })
+        await producer.send({
+            topic: 'comms.status.0',
+            messages: [
+                {value: JSON.stringify({event: 'LEAVE', device: uuid, purpose: purpose})}
+            ]
+        })
+        const wait = random.int(10000, 25000);
+        await sleep(wait);
     }
 
-    await producer.disconnect();
-    console.log("my job is done...");
+    
 }
 
 run().catch(console.error);
